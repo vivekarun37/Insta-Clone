de71bd2fea73408cb4f2314f2f2bd0f88916cdb3
vivekarun37 <vivekarun37@yahoo.com>
2024-11-16 21:04:00 -0500

    Update AuthPage.jsx

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,32 +1,29 @@
-import { Box, Container, Flex, Image, VStack } from "@chakra-ui/react"
-import Authform from "../../components/Authform/Authform"
-
+import { Container, Flex, VStack, Box, Image } from "@chakra-ui/react";
+import AuthForm from "../../components/AuthForm/AuthForm";
 
 const AuthPage = () => {
-  return (
-    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
-        <Container maxW={"container.md"} padding={0}>
-            <Flex justifyItems={'center'} alignItems={'center'} gap={10}>
-                {/* left hand side */}
-            <Box display={{base: "none",md: "block"}}>
-                <Image src="/auth.png" h={650} alt='Phone img'/>
-            </Box>
-            {/*right hand side */}
-            <VStack spacing={4} align={"stretch"}> 
-            <Authform/>
-            <Box textAlign={"center"}>Get the app.</Box>
-            <Flex gap={5} justifyContent={"center"}>
-                <Image src='/playstore.png' h={10} alt="Playstore logo"/>
-                <Image src='/microsoft.png' h={10} alt="Microsoft logo"/>
-            </Flex>
-        </VStack>
-    </Flex>
-            
-        
-</Container>
-</Flex>
+	return (
+		<Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+			<Container maxW={"container.md"} padding={0}>
+				<Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+					{/* Left hand-side */}
+					<Box display={{ base: "none", md: "block" }}>
+						<Image src='/auth.png' h={650} alt='Phone img' />
+					</Box>
 
-  );
+					{/* Right hand-side */}
+					<VStack spacing={4} align={"stretch"}>
+						<AuthForm />
+						<Box textAlign={"center"}>Get the app.</Box>
+						<Flex gap={5} justifyContent={"center"}>
+							<Image src='/playstore.png' h={"10"} alt='Playstore logo' />
+							<Image src='/microsoft.png' h={"10"} alt='Microsoft logo' />
+						</Flex>
+					</VStack>
+				</Flex>
+			</Container>
+		</Flex>
+	);
 };
 
-export default AuthPage
+export default AuthPage;
