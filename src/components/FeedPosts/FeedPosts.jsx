0db88d269ee9b42bd8f77da766f6f0a7494e9321
vivekarun37 <vivekarun37@yahoo.com>
2024-11-16 21:44:13 -0500

    Update FeedPosts.jsx

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -27,9 +27,9 @@ const FeedPosts = () => {
 			{!isLoading && posts.length === 0 && (
 				<>
 					<Text fontSize={"md"} color={"red.400"}>
-						Dayuum. Looks like you don&apos;t have any friends.
+						no friends :(
 					</Text>
-					<Text color={"red.400"}>Stop coding and go make some!!</Text>
+					
 				</>
 			)}
 		</Container>
